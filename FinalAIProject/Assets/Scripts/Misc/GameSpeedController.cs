1bc60fba4627aab7f2e99e6fc886d93c095e297c
Ben Shackman <bshackman@protonmail.com>
2019-12-10 15:02:38 -0500

    Fixed z-0 thing and added start button

---
+++ FinalAIProject/Assets/Scripts/Misc/GameSpeedController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSpeedController : MonoBehaviour
+{
+    public PlayerController playerController;
+    public float currentGameSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    public void StartGame()
+    {
+        if (ActionQueue.instance.getNumActions() > 0)
+        {
+            Time.timeScale = currentGameSpeed;
+            playerController.playerActive = true;
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = currentGameSpeed;
+    }
+}
