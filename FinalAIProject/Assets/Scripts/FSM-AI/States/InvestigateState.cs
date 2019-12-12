bfba7eb15a9c516f2784d912596817915b6e3055
jmljayhawk <jmledoux.2017@mymail.becker.edu>
2019-12-12 17:43:11 -0500

    investigate state coded

--- FinalAIProject/Assets/Scripts/FSM-AI/States/InvestigateState.cs
+++ FinalAIProject/Assets/Scripts/FSM-AI/States/InvestigateState.cs
@@ -6,6 +6,8 @@ public class InvestigateState : FSMState
 {
 
     private Enemy enemyController;
+    private float counter = 0.0f;
+
     public InvestigateState(Enemy enemy)
     {
         stateID = FSMStateID.Investigate;
@@ -16,12 +18,25 @@ public class InvestigateState : FSMState
     public override void Act(Transform player, GameObject self)
     {
         self.transform.eulerAngles = new Vector3(0, 0, self.transform.eulerAngles.z);
+        enemyController.agent.SetDestination(enemyController.playernoiseLocation);
+        if (enemyController.agent.remainingDistance <= 0.1f && counter < 360)
+        {
 
+            self.transform.eulerAngles = new Vector3(0, 0, self.transform.eulerAngles.z + 5);
+            counter += 5;
+        }
     }
 
     public override void Reason(Transform player, GameObject self)
     {
-        
+        if ( counter >= 360)
+        {
+            self.GetComponent<BaseEnemy>().SetTransition(FSMTransitions.FoundNothing);
+        }
+        if (enemyController.seePlayer == true)
+        {
+            self.GetComponent<BaseEnemy>().SetTransition(FSMTransitions.SawPlayer); //if seen, move to shoot.S
+        }
     }
 
     public override void OnStateEnter(Transform player, GameObject self)
@@ -31,6 +46,6 @@ public class InvestigateState : FSMState
 
     public override void OnStateExit(Transform player, GameObject self)
     {
-
+        counter = 0.0f;
     }
 }
