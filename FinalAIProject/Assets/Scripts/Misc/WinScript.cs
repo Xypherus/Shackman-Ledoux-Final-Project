a3e206342095d20f742c7d231cf7cf74c94797e9
Ben Shackman <Bshackman@protonmail.com>
2019-12-12 17:45:43 -0500

    Added Win Area

---
+++ FinalAIProject/Assets/Scripts/Misc/WinScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScript : MonoBehaviour
+{
+    public Text winText;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winText.text = "YOU WIN";
+            Time.timeScale = 0;
+        }
+    }
+}
